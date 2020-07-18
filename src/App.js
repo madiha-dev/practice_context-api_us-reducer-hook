b9583d4ecbb364f9b81fd5aba6147f5957758a81
madiha-dev <50271912+madiha-dev@users.noreply.github.com>
2020-07-18 16:00:41 +0000

    CounterContext Concept

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import Parent from './Parent';
-import './App.css';
 import CounterContext from './CounterContext';
 
-function App() {
-  let [count, setCount] = useState(30)
-  
-  return (
-    <CounterContext.Provider value={count}>
+function App(){
+//let [count, setCount] = useState(55);
+let countState = useState(34);
+//useState is now arry containing count, stCount variables
+
+  return(
+    //<CounterContext.Provider value={count}>
+    <CounterContext.Provider value={countState}>
       <div>
         <Parent/>
       </div>
     </CounterContext.Provider>
   );
 }
+
 
 export default App;
--- src/Child.js
+++ src/Child.js
@@ -1,25 +1,32 @@
-import React, { useContext } from 'react';
-import counterContext from './CounterContext';
+import React, {useContext} from 'react';
+import CounterContext from './CounterContext';
 
 const Child = () => {
-    let counterValue = useContext(counterContext)
+    let counterValue = useContext(CounterContext);// like a getter
+    let countState = 
+    
     console.log(counterValue);
-    return (
-        <div>
-            <h4>This is 1st child</h4>  
-            <h5>counter value is: {counterValue}</h5>
-            <button onClick={() => {++counterValue}}>Counter Btn</button>
-        </div>
-    );
+
+     return (
+         <div>
+            <p>This is first Child</p>
+            {/* <p>Child value: {counterValue}</p> */}
+            <p>Child value: {counterValue[0]}</p>
+            <button
+            onClick = {() => {counterValue[1](++counterValue[0])}}
+            >
+            Counter Btn
+            </button>
+         </div>
+     );
 }
 
 export default Child;
 
-/*Diff. b/w local and state var
+ /*Diff. b/w local and state var
  - Local var has its own data, 
         has R/W power, 
         UI never update/render
  - state var using other var data, can read only it
-    if state render again , then we get updated state var
-    
+    if state render again , then we get updated state var 
  */
